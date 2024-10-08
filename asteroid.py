0615baa831189f9003b2d82642cc385a8bd224ac
BenB32 <benbrewer2003@gmail.com>
2024-10-08 17:09:28 -0400

    Put in rest of basic func

---
+++ asteroid.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+from circleshape import CircleShape
+from constants import *
+
+
+class Asteroid(CircleShape):
+
+    def __init__(self,x,y,radius):
+        super().__init__(x,y,radius)
+
+    def draw(self,screen):
+        pygame.draw.circle(screen,"white",self.position, self.radius, 2)
+
+    def update(self,dt):
+        self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20,50)
+            r1 = self.velocity.rotate(random_angle)
+            r2 = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            ast1 = Asteroid(self.position.x,self.position.y,new_radius)
+            ast2 = Asteroid(self.position.x,self.position.y,new_radius)
+            ast1.velocity += r1 * 1.2
+            ast2.velocity += r2 * 1.2
---
+++ circleshape.py
@@ -0,0 +1,22 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+        
+        self.position = pygame.Vector2(x,y)
+        self.velocity = pygame.Vector2(0,0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def collision_with(self,collision_obj):
+        return self.position.distance_to(collision_obj.position) <= self.radius + collision_obj.radius
